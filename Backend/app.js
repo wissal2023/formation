feffd86503bda239dbace5b443652dcc14a0235f
wissal2023 <wissal.bejaoui@esprit.tn>
2025-04-07 15:50:30 +0100

    edit befor merge auth

--- Backend/app.js
+++ Backend/app.js
@@ -2,31 +2,28 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const { sequelize } = require('./db/models');
-const formationRoutes = require('./routes/formationRoutes');
-
-const sequelize = require('./config/database');
 const db = require('./db/models');
 
-app.use(express.json()); // Pour parser le JSON
+const formationRoutes = require('./routes/formationRoutes');
+const userRoutes = require('./routes/userRoute'); 
 
-app.use('/users', userRoute );
-
-app.get('/', (req,res)=> {
-    res.status(200).json({
-        status:'success',
-        message:'welcome to our api',
-    })
-})
-
+app.use(express.json()); 
 /*
 db.sequelize.sync()
   .then(() => console.log("Database schema updated"))
   .catch(err => console.error("Error updating database:", err));
 */
 
-// ✅ Use the correct variable
+// ******************* ALL ROUTES *******************
+app.get('/', (req,res)=> {
+    res.status(200).json({
+        status:'success',
+        message:'welcome to our api',
+    })
+})
+// ******************* HEAD ROUTES *******************
+app.use('/users', userRoutes);
 app.use('/formations', formationRoutes);
-
 
 
 app.use('*', (req, res) => {
@@ -35,6 +32,8 @@ app.use('*', (req, res) => {
         message: 'Page not found',
     });
 });
+
+
 
 const PORT = process.env.APP_PORT || 5000;
 app.listen(PORT, () => {
