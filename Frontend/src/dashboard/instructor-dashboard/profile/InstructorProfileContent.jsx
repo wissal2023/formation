a62ec6d0d115cd4479b3c2626879a5144219f781
wissal2023 <wissal.bejaoui@esprit.tn>
2025-05-05 17:28:16 +0100

    quiz update@

--- Frontend/src/dashboard/instructor-dashboard/profile/InstructorProfileContent.jsx
+++ Frontend/src/dashboard/instructor-dashboard/profile/InstructorProfileContent.jsx
@@ -1,6 +1,5 @@
 
 const InstructorProfileContent = ({ style }) => {
-<<<<<<< HEAD
     return (
        <div className="dashboard__content-wrap">
           <div className="dashboard__content-title">
@@ -28,32 +27,3 @@ const InstructorProfileContent = ({ style }) => {
  }
  
  export default InstructorProfileContent
-=======
-   return (
-      <div className="dashboard__content-wrap">
-         <div className="dashboard__content-title">
-            <h4 className="title">My Profile</h4>
-         </div>
-         <div className="row">
-            <div className="col-lg-12">
-               <div className="profile__content-wrap">
-                  <ul className="list-wrap">
-                     <li><span>Registration Date</span> February 28, 2026 8</li>
-                     <li><span>First Name</span> {style ? "Emily" : "John"} </li>
-                     <li><span>Last Name</span>{style ? "Hannah" : "Doe"} </li>
-                     <li><span>Username</span> instructor</li>
-                     <li><span>Email</span> example@gmail.com</li>
-                     <li><span>Phone Number</span> +1-202-555-0174</li>
-                     <li><span>Skill/Occupation</span> Application Developer</li>
-                     <li><span>Biography</span> I&apos;m the Front-End Developer for #ThemeGenix in New York, OR. I have a serious passion for UI effects, animations, and
-                        creating intuitive, dynamic user experiences.</li>
-                  </ul>
-               </div>
-            </div>
-         </div>
-      </div>
-   )
-}
-
-export default InstructorProfileContent
->>>>>>> ab0c1cbdfb0bdd0c396fb2c8dc66eb8166371774
